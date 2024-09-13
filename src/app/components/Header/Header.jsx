797a091165c583f821cbb987263c534e4f4381e1
Shinto <96878394+Shinto540@users.noreply.github.com>
2024-09-13 16:54:27 +0300

    UPDATE: 13th Sept

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,16 +1,65 @@
-"use client"; 
-import React from 'react'
+// "use client"; 
+// import React from 'react'
 
-function Header() {
-  return (
-    <div>
-      This is the header
+// function Header() {
+//   return (
+//     <div>
+//       This is the header
       
+//     </div>
+//   )
+// }
+
+// export default Header
+
+'use client'
+
+import {PopoverGroup,} from '@headlessui/react'
+import Image from 'next/image'
+
+
+
+export default function Example() {
+  
+
+  return (
+    <header className="">
+  
+        <nav className="md:flex justify-between w-full bg-blue-800 text-white p-4 sticky">
+          <div>
+          <div className="flex items-center space-x-4 p-4">
+      {/* Left side: Image */}
+      <div className="w-1/3">
+        <Image 
+          src="/assets/pictures/newlogo.png" // replace with your image path
+          alt="Description of image"
+          width={100}
+          height={100}
+          className="object-cover" // To control how the image is resized
+        />
+      </div>
+
+      {/* Right side: Text */}
+      <div className="w-2/3">
+        <h1 className="text-2xl font-bold whitespace-nowrap">Bongo Hotels</h1>
+      </div>
     </div>
+          </div>
+          <div className="md:items-center md:w-auto flex">
+            <div className="md:flex hidden">
+              <a className="block md:text-white mr-4" href="#">Browse Hotels</a>
+              <a className="block md:text-white mr-4" href="#">Best Hotels</a>
+              <a className="block md:text-white mr-4" href="#">Contact Us</a>
+              <a className="block md:text-white mr-4" href="#">About Us</a>
+            </div>
+            <div className="flex text-sm">
+              <a className="p-2 ml-2 bg-white text-teal-500 font-semibold leading-none border border-gray-100 rounded hover:border-transparent hover:bg-gray-100" href="#">Login</a>
+              <a className="p-2 ml-2 bg-teal-500 text-gray-100 font-semibold leading-none border border-teal-600 rounded hover:border-transparent hover:bg-teal-600" href="#">Sign up</a>
+            </div>
+          </div>
+        </nav>
+    </header>
   )
 }
-
-export default Header
-
 
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,45 @@
+import Image from "next/image";
+
 
 
 export default function Home() {
   return (
-    <div>
-      Entire Page
-    </div>
+    <div className=" flex items-center">
+    <section 
+      className="bg-cover bg-center py-10 w-full" 
+      style={{ backgroundImage: "url(/assets/pictures/homebg.jpg)" }}
+    >
+      <div className="container mx-auto text-left text-white">
+        <div className="flex items-center">
+          <div className="w-1/2">
+            <h1 className="text-5xl text-white font-medium mb-6">Welcome to Bongo Hotels</h1>
+            <p className="text-xl mb-12">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+              euismod odio, gravida pellentesque urna varius vitae.
+            </p>
+            <a 
+              href="#" 
+              className="bg-indigo-500 text-white py-4 px-12 rounded-full hover:bg-indigo-600"
+            >
+              Demo
+            </a>
+          </div>
+          <div className="w-1/2 pl-16">
+          <Image 
+          src="/assets/pictures/homepic.jpg" // replace with your image path
+          alt="Description of image"
+          width={1000}
+          height={1000}
+          className="object-cover" // To control how the image is resized
+        />
+          </div>
+        </div>
+      </div>
+    </section>
+<div>
+  
+</div>
+
+  </div>
   );
 }
